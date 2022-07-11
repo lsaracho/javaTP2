ca9e2184c34d317c16a40ef58ecabdfb9bd6dbeb
lsaracho <73597055+lsaracho@users.noreply.github.com>
2022-07-11 00:17:49 -0300

    Create ServicioRecetas.java

---
+++ src/main/java/com/mycompany/tprecetas/ServicioRecetas.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.tprecetas;
+
+import java.awt.List;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author VTCLUSAR
+ */
+public abstract class ServicioRecetas {
+   public abstract ArrayList obtenerIngredientes(String ing);
+   public abstract ArrayList obtenerRecetas(String rec);
+}
